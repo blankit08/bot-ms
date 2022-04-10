4026d18640a6616c242b9a22dba741012a3b376c
blankit08 <103020665+blankit08@users.noreply.github.com>
2022-04-10 00:58:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# copyright 2020-21 @Mohamed Rizad
-# Telegram @riz4d
-# Instagram @riz.4d
+# copyright 
 import telebot
 import requests
 from telebot.types import InlineKeyboardButton
 
-# Fillout Here The BotToken it gets from botfather further queries @riz4d 0n telegram
+# Fillout Here The BotToken it gets from botfather
 bot = telebot.TeleBot('5194140836:AAG4UCEB5nyI_vt1Ruik_idQ3h6caDjngr0')
 
 while True:
@@ -20,7 +18,7 @@ while True:
         @bot.message_handler(commands=['start'])
         def start_message(message):
             bot.send_message(message.chat.id,
-                             'Hey User., Bienvenido a Fakems Bot \nUsage:_\nPara generar correos electrónicos haciendo clic en el button "Generate email"\nPara actualizar su bandeja de entrada, haga clic en el botón "Refresh inbox".Después de que llegue una nueva carta, verá un botón con una línea de asunto, click en este botón para leer el message. \n\n By : @Blankito08',
+                             'Hey., Bienvenido a Fakems Bot \nUsage:_\nPara generar correos electrónicos haciendo clic en el button "Generate email"\nPara actualizar su bandeja de entrada, haga clic en el botón "Refresh inbox".Después de que llegue una nueva carta, verá un botón con una línea de asunto, click en este botón para leer el message. \n\n By : @Blankito08',
                              reply_markup=keyboard)
 
 
@@ -38,7 +36,7 @@ while True:
                 bot.send_message(message.chat.id, 'First, generate an email', reply_markup=keyboard)
             elif message.text.lower() == 'about':
                 bot.send_message(message.chat.id,
-                                 'Que es Fakems?\n- es un servicio de correo electrónico gratuito que permite recibir correo electrónico en una dirección temporal que se autodestruye después de que transcurre cierto tiempo. También es conocido por nombres como tempmail, 10minutemail, 10minmail, correo electrónico desechable, correo falso, generador de correo electrónico falso, correo quemador o correo basura\n\nPorque el correo fake se vuelve más seguro para usted?\n- El uso del correo temporal le permite proteger completamente su buzón real contra la pérdida de información personal...')
+                                'Que es Fakems?\n- es un servicio de correo electrónico gratuito que permite recibir correo electrónico en una dirección temporal que se autodestruye después de que transcurre cierto tiempo. También es conocido por nombres como tempmail, 10minutemail, 10minmail, correo electrónico desechable, correo falso, generador de correo electrónico falso, correo quemador o correo basura\n\nPorque el correo fake se vuelve más seguro para usted?\n- El uso del correo temporal le permite proteger completamente su buzón real contra la pérdida de información personal...')
             elif message.text.lower()[14] == "[":
                 email = message.text.lower()[15:message.text.lower().find("]")]
                 bkeyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -88,4 +86,3 @@ while True:
     except BaseException:
         pass
         
-# Stay tuned for more : Instagram @riz.4d
